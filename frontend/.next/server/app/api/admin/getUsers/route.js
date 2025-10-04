890701b82bceaa7e1685df0f8ab9a1d0241f9174
hashPurle <ha.shpurle@gmail.com>
2025-10-04 20:59:19 +0530

    Add admin dashboard

--- frontend/.next/server/app/api/admin/getUsers/route.js
+++ frontend/.next/server/app/api/admin/getUsers/route.js
@@ -1,6 +1,6 @@
 var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/admin/getUsers/route.js")
 R.c("server/chunks/_94ddc9c9._.js")
-R.c("server/chunks/[root-of-the-server]__11fd2199._.js")
+R.c("server/chunks/[root-of-the-server]__3eb43eca._.js")
 R.m("[project]/Digital-Mental-Health-System/frontend/.next-internal/server/app/api/admin/getUsers/route/actions.js [app-rsc] (server actions loader, ecmascript)")
 R.m("[project]/Digital-Mental-Health-System/frontend/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Digital-Mental-Health-System/frontend/src/app/api/admin/getUsers/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
 module.exports=R.m("[project]/Digital-Mental-Health-System/frontend/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Digital-Mental-Health-System/frontend/src/app/api/admin/getUsers/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
